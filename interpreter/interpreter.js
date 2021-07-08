33e5b839dde1fccaeb777bae6ce10d0abfcf7b50
matecsornyei <mate@csornyei.com>
2021-07-08 16:49:49 +0200

    Single responsibility functions in interpreter

--- interpreter/interpreter.js
+++ interpreter/interpreter.js
@@ -2,92 +2,121 @@ function bytesToNumber(accumulator, current, index) {
     return accumulator + current * Math.pow(256, index);
 }
 
-function interpreter(buffer) {
-    const message = {};
+function checkBufferLength(buffer, requiredLength) {
+    if (Buffer.byteLength(buffer) !== requiredLength) {
+        throw new Error(`Buffer must be ${requiredLength} bytes length!`);
+    }
+}
+
+function validateBuffer(buffer, bufferLength) {
     if (!Buffer.isBuffer(buffer)) {
         throw new Error("Expected a buffer!")
     }
 
-    if (Buffer.byteLength(buffer) !== 11) {
-        throw new Error("Buffer must be 11 bytes length!");
-    }
+    checkBufferLength(buffer, bufferLength);
 
-    switch (buffer[0]) {
+    if (buffer[bufferLength - 1] !== 255) {
+        throw new Error("The last byte must be 0xFF");
+    }
+}
+
+function getMessageType(byte) {
+    switch (byte) {
         case 1:
-            message["type"] = "state"
-            break;
+            return "state"
         case 2:
-            message["type"] = "error";
-            break;
+            return "error";
         default:
             throw new Error("The buffer must start with 0x01 or 0x02");
     }
+}
 
-    if (buffer[10] !== 255) {
-        throw new Error("The last byte must be 0xFF");
-    }
-
+function getIdFromBuffer(buffer) {
     const id = buffer.slice(1, 5).toJSON().data.reduce(bytesToNumber);
     if (id > 60000) {
         throw new Error("The ID can't be larger than 60000")
     }
-    message["id"] = id;
+}
 
-    const timestamp = buffer.slice(5, 9).toJSON().data.reduce(bytesToNumber);
+function getTimestampFromBuffer(buffer) {
+    return buffer.slice(5, 9).toJSON().data.reduce(bytesToNumber);
+}
+
+function padDateNumbers(number) {
+    return number < 10 ? `0${number}` : `${number}`;
+}
+
+function formatTimestampToDate(timestamp) {
     const date = new Date(timestamp * 1000);
     const year = date.getFullYear();
-    const month = date.getMonth() < 9 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
-    const day = date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
-    const hour = date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`;
-    const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
-    const seconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : `${date.getSeconds()}`;
-    const formatedDate = `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`;
-    message["timestamp"] = formatedDate;
+    const month = padDateNumbers(date.getMonth() + 1);
+    const day = padDateNumbers(date.getDate());
+    const hour = padDateNumbers(date.getHours());
+    const minutes = padDateNumbers(date.getMinutes());
+    const seconds = padDateNumbers(date.getSeconds());
+    return `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`;
+}
 
-    if (message.type === "state") {
-        switch (buffer[9]) {
-            case 1:
-                message["content"] = "red";
-                break;
-            case 2:
-                message["content"] = "yellow";
-                break;
-            case 3:
-                message["content"] = "green";
-                break;
-            case 255:
-                message["content"] = "unknown state";
-                break;
-            default:
-                throw new Error("Invalid content!");
-        };
-    } else {
-        const errors = [];
-        let contentByte = buffer[9];
-        // setting the 0b1000101 is 69 where all three error bit is on
-        // anything bigger than this is invalid
-        if (contentByte > 69 || contentByte === 0) {
+function getStateContent(byte) {
+    switch (byte) {
+        case 1:
+            return "red";
+        case 2:
+            return "yellow";
+        case 3:
+            return "green";
+        case 255:
+            return "unknown state";
+        default:
             throw new Error("Invalid content!");
-        }
-        if (contentByte >= 64) {
-            errors.push("Bandwidth Error");
-            contentByte -= 64;
-        }
-        if (contentByte >= 4) {
-            errors.push("Traffic Light Error");
-            contentByte -= 4;
-        }
-        if (contentByte >= 1) {
-            errors.push("Internal Sensor Error");
-            contentByte -= 1;
-        }
-        if (contentByte > 0) {
-            throw new Error("Invalid content!");
-        }
-        message["content"] = errors;
+    };
+}
+
+function getErrorContent(byte) {
+    const errors = [];
+    let contentByte = buffer[9];
+    // setting the 0b1000101 is 69 where all three error bit is on
+    // anything bigger than this is invalid
+    if (contentByte > 69 || contentByte === 0) {
+        throw new Error("Invalid content!");
     }
+    if (contentByte >= 64) {
+        errors.push("Bandwidth Error");
+        contentByte -= 64;
+    }
+    if (contentByte >= 4) {
+        errors.push("Traffic Light Error");
+        contentByte -= 4;
+    }
+    if (contentByte >= 1) {
+        errors.push("Internal Sensor Error");
+        contentByte -= 1;
+    }
+    if (contentByte > 0) {
+        throw new Error("Invalid content!");
+    }
+    return errors;
+}
 
-    return message;
+function getContentFromBuffer(buffer) {
+    const type = getMessageType(buffer[0]);
+    if (type === "state") {
+        return getStateContent(buffer[9]);
+    }
+    if (type === "error") {
+        return getErrorContent(buffer[9]);
+    }
+}
+
+function interpreter(buffer) {
+    validateBuffer(buffer, 11);
+
+    return {
+        type: getMessageType(buffer[0]),
+        id: getIdFromBuffer(buffer),
+        timestamp: formatTimestampToDate(getTimestampFromBuffer(buffer)),
+        content: getContentFromBuffer(buffer)
+    };
 }
 
 module.exports = interpreter;
