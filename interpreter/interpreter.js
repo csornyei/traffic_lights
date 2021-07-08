764f6f5986cd9b808a86bbc87d8b05e0efd6dab1
matecsornyei <mate@csornyei.com>
2021-07-08 16:20:34 +0200

    Interpreter without date parsing

--- interpreter/interpreter.js
+++ interpreter/interpreter.js
@@ -32,6 +32,54 @@ function interpreter(buffer) {
         throw new Error("The ID can't be larger than 60000")
     }
     message["id"] = id;
+
+    const timestamp = buffer.slice(5, 9).toJSON().data.reduce(bytesToNumber);
+    message["timestamp"] = timestamp;
+
+    if (message.type === "state") {
+        switch (buffer[9]) {
+            case 1:
+                message["content"] = "red";
+                break;
+            case 2:
+                message["content"] = "yellow";
+                break;
+            case 3:
+                message["content"] = "green";
+                break;
+            case 255:
+                message["content"] = "unknown state";
+                break;
+            default:
+                throw new Error("Invalid content!");
+        };
+    } else {
+        const errors = [];
+        let contentByte = buffer[9];
+        // setting the 0b1000101 is 69 where all three error bit is on
+        // anything bigger than this is invalid
+        if (contentByte > 69 || contentByte === 0) {
+            throw new Error("Invalid content!");
+        }
+        if (contentByte >= 64) {
+            errors.push("Bandwidth Error");
+            contentByte -= 64;
+        }
+        if (contentByte >= 4) {
+            errors.push("Traffic Light Error");
+            contentByte -= 4;
+        }
+        if (contentByte >= 1) {
+            errors.push("Internal Sensor Error");
+            contentByte -= 1;
+        }
+        if (contentByte > 0) {
+            throw new Error("Invalid content!");
+        }
+        message["content"] = errors;
+    }
+
+    return message;
 }
 
 module.exports = interpreter;
