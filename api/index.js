4686addbc9878b36f8f01b479b862d8c5f0dd5f3
matecsornyei <mate@csornyei.com>
2021-07-09 21:24:24 +0200

    Basic route and mock data

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express, { json } = require("express");
+
+const router = require("./routes");
+
+const app = express();
+
+app.use(json());
+app.use("/api", router);
+app.all("*", (_, res) => {
+    res.status(404).send({
+        error: "Page not found"
+    });
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
